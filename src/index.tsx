c3c6a05b458aa839a77ba848e1f5a907fcb2a130
Nonna <nonnagrigorovich@gmail.com>
2023-03-11 05:17:29 +0200

    add new file Content.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Title from './Title'
-
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Molestias accusamus quaerat deserunt excepturi aliquam
-                consequuntur iste esse quo consequatur dignissimos alias,
-                perspiciatis adipisci temporibus at, vitae obcaecati sequi
-                dolore? Sequi.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-                vitae quaerat est ex exercitationem amet error ipsum
-                accusantium, odio possimus aut, dolorem minima minus, a hic id
-                cupiditate pariatur quis!
-            </p>
-        </React.Fragment>
-    )
-}
+import Content from './Content'
 
 const App = () => {
     return (
         <>
             <Title title="React" year={2023} />
             <Title title="App" year={2022} />
-            <Content />
+            <Content text1="Hello world text1" text2="Hello world text2" />
         </>
     )
 }
