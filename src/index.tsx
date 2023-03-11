97ca04680b456e12ce12351e1a94f8400dbda450
Nonna <nonnagrigorovich@gmail.com>
2023-03-11 04:34:43 +0200

    add React.Fragment and add new file Title.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,38 @@ import ReactDOM from 'react-dom/client'
 // with JSX
 
 //PascalCase - назва починаеться з великої букви (викликається як тег)
-const App = () => {
+
+const Title = () => {
     return <h1>Hello App component</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias accusamus quaerat deserunt excepturi aliquam
+                consequuntur iste esse quo consequatur dignissimos alias,
+                perspiciatis adipisci temporibus at, vitae obcaecati sequi
+                dolore? Sequi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                vitae quaerat est ex exercitationem amet error ipsum
+                accusantium, odio possimus aut, dolorem minima minus, a hic id
+                cupiditate pariatur quis!
+            </p>
+        </React.Fragment>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
