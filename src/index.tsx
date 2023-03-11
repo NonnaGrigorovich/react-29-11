288ad4b60011d7d78cff70f41fab30c9418c1b7c
Nonna <nonnagrigorovich@gmail.com>
2023-03-11 22:12:14 +0200

    cleaned up index.tsx; delete files content.tsx and title.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Title from './Title'
-import Content from './Content'
-
-const App = () => {
-    return (
-        <>
-            <Title title="React" year={2023} />
-            <Title title="App" year={2022} />
-            <Content text1="Hello world text1" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+
+root.render(<React.StrictMode></React.StrictMode>)
