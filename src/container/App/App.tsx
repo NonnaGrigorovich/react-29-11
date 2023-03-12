7432e94edcdbb55d7223e648d890ece8341c334b
Nonna <nonnagrigorovich@gmail.com>
2023-03-12 02:12:38 +0200

    add CssBaseline

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,6 +1,7 @@
 import Header from "container/Header/Header"
 import Main from "container/Main/Main"
 import Footer from "container/Footer/Footer"
+import CssBaseline from '@mui/material/CssBaseline';
 
 // tsrafce - швидкий виклик
 type Props = {}
@@ -8,6 +9,7 @@ type Props = {}
 const App = (props: Props) => {
   return (
     <div>
+        <CssBaseline />
         <Header />
         <Main />
         <Footer />
