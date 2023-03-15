8afb2f9ca42c3d8e71b096ea8c69df4798b115aa
Nonna <nonnagrigorovich@gmail.com>
2023-03-16 01:21:33 +0200

    add productsArray.map in ProductList.tsx

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,5 +1,6 @@
 import { Grid, Typography } from '@mui/material'
 import ProductListItem from './ProductListItem'
+import productsArray from 'utils/productsArray'
 
 type Props = {}
 
@@ -19,33 +20,17 @@ const ProductList = (props: Props) => {
             </Typography>
 
             <Grid container spacing={4}>
-                <Grid item xs={12} sm={4}>
-                    <ProductListItem
-                        title="iPhone 14 Pro"
-                        description="This is iPhone 14 Pro"
-                        type="phone"
-                        capacity="256"
-                        price={1200}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <ProductListItem
-                        title="iPhone 13 Pro"
-                        description="This is iPhone 13 Pro"
-                        type="phone"
-                        capacity="128"
-                        price={800}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <ProductListItem
-                        title="iPhone 12 Pro"
-                        description="This is iPhone 12 Pro"
-                        type="phone"
-                        capacity="64"
-                        price={500}
-                    />
-                </Grid>
+                {productsArray.map(({title, description, type, capacity, price}) => (
+                    <Grid item xs={12} sm={4}>
+                        <ProductListItem
+                            title={title}
+                            description={description}
+                            type={type}
+                            capacity={capacity}
+                            price={price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
