1cfd42a44b6660d24d33c2478d2622faf538b086
Nonna <nonnagrigorovich@gmail.com>
2023-03-16 02:31:16 +0200

    add destructuring in class ProductListItem

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -19,24 +19,21 @@ type Props = {
 
 class ProductListItem extends Component<Props> {
     render() {
+        const { title, description, type, capacity, price, image } = this.props
         return (
             <Card variant="outlined" className="product">
                 <CardContent>
                     <div className="product-image">
-                        <img src={this.props.image} alt="" />
+                        <img src={image} alt="" />
                     </div>
-                    <h4 className="product-title">{this.props.title}</h4>
-                    <div className="product-description">
-                        {this.props.description}
-                    </div>
+                    <h4 className="product-title">{title}</h4>
+                    <div className="product-description">{description}</div>
+                    <div className="product-features">Type: {type}</div>
                     <div className="product-features">
-                        Type: {this.props.type}
-                    </div>
-                    <div className="product-features">
-                        Capacity: {this.props.capacity}Gb
+                        Capacity: {capacity}Gb
                     </div>
                     <div className="product-price">
-                        Price: <strong>${this.props.price}</strong>
+                        Price: <strong>${price}</strong>
                     </div>
                     <div className="product-quantity">
                         <Button variant="outlined">-</Button>
