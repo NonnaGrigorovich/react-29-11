361b9404c164c39c37a277a6149db8b93e94ac21
Nonna <nonnagrigorovich@gmail.com>
2023-03-16 20:42:59 +0200

    add cartData to App

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -3,11 +3,22 @@ import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import { useState } from 'react'
 
 // tsrafce - швидкий виклик
 type Props = {}
 
+type CartData = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = (props: Props) => {
+    const [CharacterData, setCartData] = useState<CartData>({
+        totalCount: 10,
+        totalPrice: 100,
+    })
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
