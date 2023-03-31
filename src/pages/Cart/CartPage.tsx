48e02c402b9eea3ad70c4470b406a0a3a4e970f3
Nonna <nonnagrigorovich@gmail.com>
2023-03-31 04:59:57 +0300

    add CartTotal and CartProductList

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,39 +1,22 @@
-import { Typography } from "@mui/material"
-import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+import { Typography } from '@mui/material'
+import CartTotal from 'components/CartTotal/CartTotal'
+import CartProductList from 'components/CartProductList/CartProductList'
 
-type Props = {productsInCart: {
-    [id: number]: number
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
 }
-productsObject?: {
-    [id: number]: Product
-}}
-const CartPage = ({
-    productsInCart,
-    productsObject = getProductsObject(productsArray),
-}: Props) => {
+
+const CartPage = ({ productsInCart }: Props) => {
     return (
         <>
-            <Typography variant="h4" component={'h1'}>CartPage</Typography>
+            <Typography variant="h4" component={'h1'}>
+                CartPage
+            </Typography>
             <div>
-                <div>
-                    {Object.keys(productsInCart).map((productId) => (
-                        <div key={productId}>
-                            {productsObject[parseInt(productId)].title}:{' '}
-                            {productsInCart[parseInt(productId)]}:
-                            {productsObject[parseInt(productId)].price}
-                        </div>
-                    ))}
-                </div>
-                <div>
-                    Total: ${' '}
-                    {Object.keys(productsInCart).reduce(
-                        (total, productId) =>
-                            total +
-                            productsObject[parseInt(productId)].price *
-                                productsInCart[parseInt(productId)],
-                        0
-                    )}
-                </div>
+                <CartProductList productsInCart={productsInCart} />
+                <CartTotal productsInCart={productsInCart} />
             </div>
         </>
     )
