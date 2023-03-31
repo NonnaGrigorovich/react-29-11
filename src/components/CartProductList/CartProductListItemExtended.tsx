30122164d0b803bdcca50fba24fe1a7b3813135f
Nonna <nonnagrigorovich@gmail.com>
2023-03-31 23:02:24 +0300

    add quantity

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -1,6 +1,7 @@
 import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Quantity from 'components/Quantity/Quantity'
 
 type Props = {
     product: Product
@@ -23,6 +24,9 @@ const CartProductListItemExtended = ({
                     <h4>{product.title}</h4>
                     <p>Price for one item: ${product.price}</p>
                     <p>Count:{productCount}</p>
+                    <Quantity count={productCount} 
+                    onDecrement={() => console.log('test')}
+                    onIncrement={() => console.log('test')}/>
                     <Button
                         variant="outlined"
                         onClick={() => removeProductFromCart(product.id)}
--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,12 +1,7 @@
-import {
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    TextField,
-} from '@mui/material'
+import { Button, Card, CardActions, CardContent } from '@mui/material'
 import { useState } from 'react'
 import './ProductListItem.scss'
+import Quantity from 'components/Quantity/Quantity'
 
 type Props = {
     id: number
@@ -53,19 +48,11 @@ const ProductListItem = ({
                     Price: <strong>${price}</strong>
                 </div>
 
-                <div className="product-quantity">
-                    <Button
-                        variant="outlined"
-                        onClick={() => onDecrement()}
-                        disabled={count <= 1}
-                    >
-                        -
-                    </Button>
-                    <TextField size="small" value={count} />
-                    <Button variant="outlined" onClick={() => onIncrement()}>
-                        +
-                    </Button>
-                </div>
+                <Quantity
+                    count={count}
+                    onDecrement={onDecrement}
+                    onIncrement={onIncrement}
+                />
                 <CardActions className="btns-wrap">
                     <Button
                         variant="outlined"
